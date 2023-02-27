f2b8586b9d40b83cdc8e280a0029603aa6aaba3b
Michal Korcz <mkorcz01@gmail.com>
2023-02-27 21:33:34 +0100

    Added switch menu

--- src/main/java/pl/mkorcz/atm/AtmApplication.java
+++ src/main/java/pl/mkorcz/atm/AtmApplication.java
@@ -3,11 +3,39 @@ package pl.mkorcz.atm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class AtmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtmApplication.class, args);
+
+		int balance = 0;
+		int withdraw;
+		System.out.println("Welcome to your ATM: ");
+		System.out.println("Please, choose the number: ");
+		System.out.println("1. Withdraw");
+		System.out.println("2. Deposit");
+		System.out.println("3. Check your account balance");
+		System.out.println("4. EXIT");
+
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+
+		switch (n) {
+			case 1:
+				System.out.println("How much money would you like to withdraw?");
+				withdraw = scanner.nextInt();
+				if (balance >= withdraw) {
+					balance -= withdraw;
+					System.out.println("Here's your money");
+				} else {
+					System.out.println("You don't have enough money to withdraw!");
+				}
+				break;
+		}
 	}
 
 }
