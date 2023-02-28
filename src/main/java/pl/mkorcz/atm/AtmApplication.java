057bb13d16aa0a6c1ed6e697f573c9544a158a2c
Michal Korcz <mkorcz01@gmail.com>
2023-02-28 21:54:28 +0100

    added case 2 at atm menu

--- src/main/java/pl/mkorcz/atm/AtmApplication.java
+++ src/main/java/pl/mkorcz/atm/AtmApplication.java
@@ -8,44 +8,53 @@ import java.util.Scanner;
 @SpringBootApplication
 public class AtmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AtmApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(AtmApplication.class, args);
 
-		int balance = 0;
-		int withdraw;
-		System.out.println("Welcome to your ATM: ");
-		System.out.println("Please, choose the number: ");
-		System.out.println("1. Withdraw");
-		System.out.println("2. Deposit");
-		System.out.println("3. Check your account balance");
-		System.out.println("4. EXIT");
+        int balance = 0;
+        int withdraw, deposit;
 
-		Scanner scanner = new Scanner(System.in);
 
-		int n = scanner.nextInt();
 
-		switch (n) {
-			case 1:
-				System.out.println("How much money would you like to withdraw?");
-				withdraw = scanner.nextInt();
-				if (balance >= withdraw) {
-					balance -= withdraw;
-					System.out.println("Here's your money");
-				} else {
-					System.out.println("You don't have enough money to withdraw!");
-				}
-				break;
-			case 2:
-				System.out.println("deposit");
-				break;
-			case 3:
-				System.out.println("checking");
-				break;
-			case 4:
-				System.out.println("exit");
-				break;
+        while (true) {
+            System.out.println("Welcome to your ATM: ");
+            System.out.println("Please, choose the number: ");
+            System.out.println("1. Withdraw");
+            System.out.println("2. Deposit");
+            System.out.println("3. Check your account balance");
+            System.out.println("4. EXIT");
 
-		}
-	}
+			Scanner scanner = new Scanner(System.in);
+
+			int n = scanner.nextInt();
+
+            switch (n) {
+                case 1:
+                    System.out.println("How much money would you like to withdraw?");
+                    withdraw = scanner.nextInt();
+                    if (balance >= withdraw) {
+                        balance -= withdraw;
+                        System.out.println("Here's your money");
+                    } else {
+                        System.out.println("You don't have enough money to withdraw!");
+                    }
+                    break;
+                case 2:
+                    System.out.println("How much money would you like to put into your bank account?");
+					deposit = scanner.nextInt();
+
+					balance += deposit;
+					System.out.println("added " + deposit + "$ to your bank account");
+					System.out.println();
+                    break;
+                case 3:
+                    System.out.println("checking");
+                    break;
+                case 4:
+                    System.out.println("exit");
+                    break;
+            }
+        }
+    }
 
 }
